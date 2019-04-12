f5fc05dda181f7396e9a442e9afa107c687fd096
StevenTheElder <49321677+StevenTheElder@users.noreply.github.com>
2019-04-12 21:41:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function getRandomPosInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+document.body.style.backgroundColor = "grey";
+function updatePage(xkcd) {
+  fetch('https://xkcd.com/' + xkcd + '/info.0.json').then(function(response) {
+    response.json().then(function(data) {
+      var num = data.num;
+      var img = data.img;
+      var title = data.safe_title;
+      var alt = data.alt;
+      var targetTitle = document.getElementById('title');
+      var targetImg = document.getElementById('xkcd');
+      var targetAlt = document.getElementById('alt');
+      targetImg.src = img;
+      targetTitle.href = "https://xkcd.com/" + num;
+      targetTitle.innerText = title;
+      targetAlt.textContent = alt;
+    });
+  });
+}
+
+fetch('https://xkcd.com/info.0.json').then(function(response) {
+  response.json().then(function(data) {
+    var num = data.num;
+    var x = getRandomPosInt(num);
+    updatePage(x);
+  });
+});
